ca14940bb5bf920be466444faa15b9c933de6b2f
Conner Alegre <cga0@pm.me>
2018-10-24 15:37:43 -0600

    Complete project

--- Tic Tac Toe/TicTacToe-Watch Extension/TicTacToeInterfaceController.swift
+++ Tic Tac Toe/TicTacToe-Watch Extension/TicTacToeInterfaceController.swift
@@ -9,23 +9,92 @@
 import WatchKit
 import Foundation
 
-
 class TicTacToeInterfaceController: WKInterfaceController {
-
+    // MARK: - Properties
+    private var game = Game() {
+        didSet {
+            updateViews()
+        }
+    }
+    private var buttons: [WKInterfaceButton] = []
+    /*
+     0,0 1,0 2,0
+     0,1 1,1 2,1
+     0,2 1,2 2,2
+     */
+    // MARK: - IBOutlets
+    @IBOutlet var statusLabel: WKInterfaceLabel!
+    @IBOutlet var buttonZeroZero: WKInterfaceButton!
+    @IBOutlet var buttonOneZero: WKInterfaceButton!
+    @IBOutlet var buttonTwoZero: WKInterfaceButton!
+    @IBOutlet var buttonZeroOne: WKInterfaceButton!
+    @IBOutlet var buttonOneOne: WKInterfaceButton!
+    @IBOutlet var buttonTwoOne: WKInterfaceButton!
+    @IBOutlet var buttonZeroTwo: WKInterfaceButton!
+    @IBOutlet var buttonOneTwo: WKInterfaceButton!
+    @IBOutlet var buttonTwoTwo: WKInterfaceButton!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
+        buttons = [buttonZeroZero, buttonOneZero, buttonTwoZero,
+                   buttonZeroOne, buttonOneOne, buttonTwoOne,
+                   buttonZeroTwo, buttonOneTwo, buttonTwoTwo]
+        updateViews()
     }
     
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
+    // MARK: - IBActions
+    @IBAction func resetButtonTapped() {
+        game.restart()
+        self.buttons.forEach { $0.setTitle("") }
+    }
+    @IBAction func buttonZeroZeroTapped() {
+        mark(coordinate: (0, 0), button: buttonZeroZero)
+    }
+    @IBAction func buttonOneZeroTapped() {
+        mark(coordinate: (1, 0), button: buttonOneZero)
+    }
+    @IBAction func buttonTwoZeroTapped() {
+        mark(coordinate: (2, 0), button: buttonTwoZero)
+    }
+    @IBAction func buttonZeroOneTapped() {
+        mark(coordinate: (0, 1), button: buttonZeroOne)
+    }
+    @IBAction func buttonOneOneTapped() {
+        mark(coordinate: (1, 1), button: buttonOneOne)
+    }
+    @IBAction func buttonTwoOneTapped() {
+        mark(coordinate: (2, 1), button: buttonTwoOne)
+    }
+    @IBAction func buttonZeroTwoTapped() {
+        mark(coordinate: (0, 2), button: buttonZeroTwo)
+    }
+    @IBAction func buttonOneTwoTapped() {
+        mark(coordinate: (1, 2), button: buttonOneTwo)
+    }
+    @IBAction func buttonTwoTwoTapped() {
+        mark(coordinate: (2, 2), button: buttonTwoTwo)
     }
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    // MARK: - Private Functions
+    private func mark(coordinate: Coordinate, button: WKInterfaceButton) {
+        guard game.winningPlayer?.stringValue == nil else { return }
+        do {
+            button.setTitle(game.activePlayer?.stringValue)
+            updateViews()
+            try game.makeMark(at: coordinate)
+        } catch {
+            NSLog("Illegal move")
+        }
     }
-
+    
+    private func updateViews() {
+        if let winner = game.winningPlayer?.stringValue {
+            statusLabel.setText("Player \(winner) won!")
+        } else if let player = game.activePlayer?.stringValue {
+            statusLabel.setText("Player \(player)'s turn")
+        } else if game.isOver {
+            statusLabel.setText("Cat's game!")
+        }
+    }
+    
 }
