9b76c7d6c44ea60b3709a778e50ea3dcd5386673
Conner Alegre <cga0@pm.me>
2018-10-24 14:08:41 -0600

    Rename interface controller

--- Tic Tac Toe/TicTacToe-Watch Extension/TicTacToeInterfaceController.swift
+++ Tic Tac Toe/TicTacToe-Watch Extension/TicTacToeInterfaceController.swift
@@ -10,7 +10,7 @@ import WatchKit
 import Foundation
 
 
-class InterfaceController: WKInterfaceController {
+class TicTacToeInterfaceController: WKInterfaceController {
 
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
